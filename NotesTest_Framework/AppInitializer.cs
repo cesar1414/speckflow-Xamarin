91962ac8a1db341ac204fd73b5744d97c547c98b
Cesar Calvi Lujan <Cesar Calvi@jalasoft.local>
2019-09-12 11:46:58 -0400

    first version

---
+++ NotesTest_Framework/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace NotesTest_Framework
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .Debug()
+                    .InstalledApp("com.companyname.Notes")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ NotesTest_Framework/Feature/SpecFlowFeature1.feature
@@ -0,0 +1,13 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Add item in EAXamarinApp
+	Given I create a new item by following values
+		| key         | values      |
+		| Title       | patito 123  |
+		| Description | patito nada | 
+	When I press add button
+	Then the item should be displayed
+
